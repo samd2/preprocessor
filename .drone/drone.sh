f44ccbce313afccacb887412ebe6d9f18815571e
sdarwin <samuel.d.darwin@gmail.com>
2021-01-20 21:05:23 +0000

    add drone config

--- .drone/drone.sh
+++ .drone/drone.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
-set -ex
+# Copyright 2020 Rene Rivera, Sam Darwin
+# Distributed under the Boost Software License, Version 1.0.
+# (See accompanying file LICENSE.txt or copy at http://boost.org/LICENSE_1_0.txt)
+
+set -e
 export TRAVIS_BUILD_DIR=$(pwd)
 export DRONE_BUILD_DIR=$(pwd)
 export TRAVIS_BRANCH=$DRONE_BRANCH
@@ -28,9 +32,40 @@ echo '==================================> SCRIPT'
 echo "using $TOOLSET : : $COMPILER ;" > ~/user-config.jam
 ./b2 libs/preprocessor/test -j 3 toolset=$TOOLSET cxxstd=$CXXSTD $ADDRMD
 
-fi
+elif [ "$DRONE_JOB_BUILDTYPE" == "ebf47d6801-2ef7ed6698" ]; then
 
-if [ "$DRONE_JOB_BUILDTYPE" == "cmake-install-script" ]; then
+echo '==================================> INSTALL'
+
+BOOST_BRANCH=develop && [ "$TRAVIS_BRANCH" == "master" ] && BOOST_BRANCH=master || true
+cd ..
+git clone -b $BOOST_BRANCH --depth 1 https://github.com/boostorg/boost.git boost-root
+cd boost-root
+git submodule update --init tools/boostdep
+cp -r $TRAVIS_BUILD_DIR/* libs/preprocessor
+python tools/boostdep/depinst/depinst.py preprocessor
+./bootstrap.sh
+./b2 headers
+
+echo '==================================> SCRIPT'
+
+mkdir __build__ && cd __build__
+cmake -DBOOST_ENABLE_CMAKE=1 -DBoost_VERBOSE=1 -DBOOST_INCLUDE_LIBRARIES=preprocessor ..
+ctest --output-on-failure -R boost_preprocessor
+
+elif [ "$DRONE_JOB_BUILDTYPE" == "41e711e69e-fda7c76df5" ]; then
+
+echo '==================================> INSTALL'
+
+true
+
+echo '==================================> SCRIPT'
+
+cd test/cmake_subdir_test && mkdir __build__ && cd __build__
+cmake ..
+cmake --build .
+cmake --build . --target check
+
+elif [ "$DRONE_JOB_BUILDTYPE" == "ebf47d6801-53809b8261" ]; then
 
 echo '==================================> INSTALL'
 
@@ -53,38 +88,5 @@ cd ../libs/preprocessor/test/cmake_install_test && mkdir __build__ && cd __build
 cmake -DCMAKE_INSTALL_PREFIX=~/.local ..
 cmake --build .
 cmake --build . --target check
-
-elif [ "$DRONE_JOB_BUILDTYPE" == "cmake-subdir-script" ]; then
-
-echo '==================================> INSTALL'
-
-true
-
-echo '==================================> SCRIPT'
-
-cd test/cmake_subdir_test && mkdir __build__ && cd __build__
-cmake ..
-cmake --build .
-cmake --build . --target check
-
-elif [ "$DRONE_JOB_BUILDTYPE" == "cmake-test-script" ]; then
-
-echo '==================================> INSTALL'
-
-BOOST_BRANCH=develop && [ "$TRAVIS_BRANCH" == "master" ] && BOOST_BRANCH=master || true
-cd ..
-git clone -b $BOOST_BRANCH --depth 1 https://github.com/boostorg/boost.git boost-root
-cd boost-root
-git submodule update --init tools/boostdep
-cp -r $TRAVIS_BUILD_DIR/* libs/preprocessor
-python tools/boostdep/depinst/depinst.py preprocessor
-./bootstrap.sh
-./b2 headers
-
-echo '==================================> SCRIPT'
-
-mkdir __build__ && cd __build__
-cmake -DBOOST_ENABLE_CMAKE=1 -DBoost_VERBOSE=1 -DBOOST_INCLUDE_LIBRARIES=preprocessor ..
-ctest --output-on-failure -R boost_preprocessor
 
 fi
